0f3fff725c13035bf22f250db484a6f09c670e7c
akemimendes <akemi.mendes@hotmail.com>
2025-08-02 08:47:26 -0300

    criacao projeto e tela menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Nav } from './components/nav/nav';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: Nav
+
+    }
+];
