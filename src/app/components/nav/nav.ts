5ae8c960a9c2efdddfdec8d8f9f204dda6b4ca1f
akemimendes <akemi.mendes@hotmail.com>
2025-08-02 08:16:49 -0300

    Criacção do projeto e construção do menu

---
+++ src/app/components/nav/nav.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-nav',
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css'
+})
+export class Nav {
+
+}
